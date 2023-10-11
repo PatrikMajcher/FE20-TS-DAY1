ed028c191293f06863b64e8b2cf3e857beabca9c
sahrajoy <sahra.stursa@live.at>
2023-10-11 13:25:31 +0200

    basic exercise SJS

--- BasicSJS/script.js
+++ BasicSJS/script.js
@@ -2,12 +2,56 @@
 /* Basic ex 1
 Make a multiplication table using TypeScript loops from an array with number values 1 to 10*/
 function multTab() {
-    for (let i = 0; i < 11; i++) {
+    for (let iA = 1; iA < 11; iA++) {
         let result = document.getElementById("multiplication-table");
-        let sum = i + 1;
         result.innerHTML += `
-            <p>${i} X 1 = ${sum}</p>
+        <hr>
         `;
+        for (let iB = 1; iB < 11; iB++) {
+            let result = document.getElementById("multiplication-table");
+            let sum = iB * iA;
+            result.innerHTML += `
+                <p>${iB} X ${iA} = ${sum}</p>
+            `;
+        }
     }
 }
 multTab();
+/*Basic ex 2
+1. Print your full name in the browser (you will have an object that has 2 properties fName and lName).
+2. Print your first name 10 times in the browser using a forEach loop (don't forget that forEach loop work on arrays, so you can put the object in an array and repeat it).
+3. A function should be triggered 5 seconds after opening the page that prints your last name in the console 10 times.*/
+let obj = [{
+        fName: "Sahra Joy",
+        lName: "Stursa"
+    }];
+for (let i = 0; i < 10; i++) {
+    obj.forEach(name => {
+        let result = document.getElementById("fName");
+        result.innerHTML += `
+            <p>${name.fName}</p>
+        `;
+    });
+}
+;
+function lName() {
+    for (let i = 0; i < 10; i++) {
+        obj.forEach(name => {
+            console.log(name.lName);
+        });
+    }
+    ;
+}
+;
+setTimeout(lName, 5000);
+/*Basic ex 3
+Create an array of names, this array should only accept strings.
+1. Print only the index of the elements.
+2. Print only the values.
+3. Print both index and values.*/
+let names = ["John", "Jane", "Mark"];
+names.forEach((e, i) => {
+    console.log(i);
+    console.log(e);
+    console.log(`${i} ${e}`);
+});
--- BasicSJS/script.ts
+++ BasicSJS/script.ts
@@ -1,13 +1,62 @@
 /* Basic ex 1
 Make a multiplication table using TypeScript loops from an array with number values 1 to 10*/
 
-function multTab() : void {
-    for(let i = 0; i < 11; i++){
+function multTab(): void {
+    for (let iA = 1; iA < 11; iA++) {
         let result = document.getElementById("multiplication-table") as HTMLElement;
-        let sum = i + 1;
         result.innerHTML += `
-            <p>${i} X 1 = ${sum}</p>
+        <hr>
         `;
+        for (let iB = 1; iB < 11; iB++) {
+            let result = document.getElementById("multiplication-table") as HTMLElement;
+            let sum = iB*iA;
+            result.innerHTML += `
+                <p>${iB} X ${iA} = ${sum}</p>
+            `;
+        }
     }
 }
 multTab();
+
+/*Basic ex 2
+1. Print your full name in the browser (you will have an object that has 2 properties fName and lName).
+2. Print your first name 10 times in the browser using a forEach loop (don't forget that forEach loop work on arrays, so you can put the object in an array and repeat it). 
+3. A function should be triggered 5 seconds after opening the page that prints your last name in the console 10 times.*/
+
+let obj: Array <{fName : string, lName: string}> = [{
+    fName: "Sahra Joy",
+    lName: "Stursa"
+}];
+
+for(let i = 0; i < 10; i++){
+    obj.forEach(name => {
+        let result = document.getElementById("fName") as HTMLElement;
+        result.innerHTML += `
+            <p>${name.fName}</p>
+        `
+    })
+};
+
+function lName(){
+    for(let i = 0; i < 10; i++){
+        obj.forEach(name => {
+            console.log(name.lName);
+        })
+    };
+};
+
+setTimeout(lName, 5000);
+
+/*Basic ex 3
+Create an array of names, this array should only accept strings.
+1. Print only the index of the elements.
+2. Print only the values. 
+3. Print both index and values.*/
+
+let names : Array<string> = ["John", "Jane", "Mark"];
+names.forEach((e, i) => {
+    console.log(i);
+    console.log(e);
+    console.log(`${i} ${e}`);
+});
+
