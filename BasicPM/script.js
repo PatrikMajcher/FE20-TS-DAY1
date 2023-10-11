44c8d9c98db5f8bf04aa8f37e5fc90b120bd5f3c
PatrikMajcher <majcher.patrik@gmail.com>
2023-10-11 11:51:51 +0200

    ex1 done

---
+++ BasicPM/script.js
@@ -0,0 +1,12 @@
+"use strict";
+// ex1
+let myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let i = 0; i < myNumbers.length; i++) {
+    for (let j = 1; j <= 10; j++) {
+        let result = myNumbers[i] * j;
+        let ex1 = document.getElementById("ex1");
+        ex1.innerHTML += `${myNumbers[i]} * ${j}= ${result}
+        <hr>
+        `;
+    }
+}
